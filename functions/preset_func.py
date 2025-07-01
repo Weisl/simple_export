c0a5d801553a02edbb71987a63da5fd88dfa09c7
Weisl <weisl12@hotmail.de>
2025-07-01 10:03:50 +0200

    update

--- functions/preset_func.py
+++ functions/preset_func.py
@@ -123,7 +123,8 @@ def assign_preset(exporter, preset_path):
 
     # Parse the preset file and remove filepath
     preset_properties = parse_preset_file(preset_path)
-    if not preset_properties:
+
+    if preset_properties:
         del preset_properties['filepath']
 
     # Apply the properties to the exporter
